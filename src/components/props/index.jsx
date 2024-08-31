e162ada7fb448d58bfbd097aeded52f8e0f94c58
lyanbrito <lyanbrito613@gmail.com>
2024-08-31 09:40:54 -0300

    chore:commented-unused-files

--- src/components/props/index.jsx
+++ src/components/props/index.jsx
@@ -1,16 +1,16 @@
-import "./style.css"
+// import "./style.css"
 
-export default function Card({ type, num, name, sprite, imgAlt, height }) {
-    return (
-      <card>
-        <img src={sprite} alt={imgAlt} />
-        <div className="text-wrapper">
-        <p>{num}</p>
-        <p>{name}</p>
-        </div>
-        <p>{type}</p>
-        <p>{height}</p>
+// export default function Card({ type, num, name, sprite, imgAlt, height }) {
+//     return (
+//       <card>
+//         <img src={sprite} alt={imgAlt} />
+//         <div className="text-wrapper">
+//         <p>{num}</p>
+//         <p>{name}</p>
+//         </div>
+//         <p>{type}</p>
+//         <p>{height}</p>
 
-      </card>
-    );
-  }
+//       </card>
+//     );
+//   }
