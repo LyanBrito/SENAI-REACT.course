1793d788b09e0d2ad1b8caa370d73f04ed54e3a2
lyanbrito <lyanbrito613@gmail.com>
2024-08-31 16:34:04 -0300

    feat:created-brand-component

---
+++ src/components/brand/index.jsx
@@ -0,0 +1,36 @@
+// import American from "./img/american-express.svg";
+// import Alelo from "./img/alelo.svg";
+// import Master from "./img/master-card.svg";
+// import Visa from "./img/visa.svg";
+// import { BrandImage } from "./style";
+
+
+//  export default function Branch({brand}) {
+//   switch (brand) {
+//     case "american":
+//       return <img src={American} alt="american-service" />;
+//     case "master":
+//       return <img src={Master} alt="master" />;
+//     case "alelo":
+//       return <img src={Alelo} alt="alelo" />;
+//     case "visa":
+//     return <img src={Visa} alt="visa" />;
+//     default: return <h1>alalall</h1>;
+//   }
+// }
+
+// const brandImgs = {
+//   american: American,
+//   alelo: Alelo,
+//   master: Master,
+//   visa: Visa,
+// };
+
+// export default function Brand({ brand }) {
+//   const ImageSrc = brandImgs[brand];
+//   return ImageSrc ? (
+//     <BrandImage src={ImageSrc} alt={`Imagem da bandeira ${brand}`} />
+//   ) : (
+//     <h1>Selecione uam prop decente</h1>
+//   );
+// }
---
+++ src/components/brand/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const BrandImage = styled.img`
+  width: 230px;
+  height: 230px;
+`;
